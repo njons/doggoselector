e2f24b2f00b47cec96fa856acd86f4e32b51a0f4
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2019-02-02 13:29:40 +0100

    add files

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "react-dom";
+import App from "./components/App.js";
+
+// const App = () => <h1>Hello World</h1>;
+
+render(<App />, document.getElementById("root"));
